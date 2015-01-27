a88d869fc9e1083795e8208d7e7c31563280fd2d
Forrest Scofield <fscofield@cbinsights.com>
2015-01-27 14:02:11 -0500

    add optional revision history as cli flag

--- unused.sh
+++ unused.sh
@@ -3,31 +3,36 @@
 EXT='php'
 DIR='.'
 OMIT=''
-while getopts ":d:e:o:" opt
+REV="0"
+while getopts ":d:e:o:r" opt
    do
      case $opt in
         d ) DIR=$OPTARG;;
         e ) EXT=$OPTARG;;
         o ) OMIT=$OPTARG;;
+        r ) REV="1";;
      esac
 done
 
 for subdir in $OMIT;do
-    OMIT_PATH+=" -path $subdir -o "
+    OMIT_PATH+=" -not ( -path $DIR/$subdir -prune ) "
 done
 
-OMIT_PATH=${OMIT_PATH:0:${#OMIT_PATH}-3}
-
+FILES=$(find ${DIR} $OMIT_PATH -name "*.${EXT}" )
+echo "searching for matching files..."
+echo "excluding files in $OMIT..."
+echo ${#FILES} files collected...
 echo searching "for unused .${EXT} files in" `cd "${DIR}"; pwd`...
-echo "excluding files in $OMIT"
-FILES=$(find ${DIR} -not \( ${OMIT_PATH} \) -name "*.${EXT}" )
 
 for FILE in $FILES; do
     short_name=`basename $FILE`
     filename="${short_name%.*}"
     git grep --quiet $short_name 1>/dev/null
     if [ "$?" == "1" ]; then
-        rev_history=$(git log --pretty=format:"%h%x09%an%x09%ad%x09$a" -1 -- $FILE | awk '{printf "%3s %1s %3s %3s\n", $5, $8, $2, $3}')
-        echo "Should delete: $FILE (last edit: $rev_history)"
+        if [ "$REV" == "1" ]; then
+            history=$(git log --pretty=format:"%h%x09%an%x09%ad%x09$a" -1 -- $FILE | awk '{printf "%3s %1s %3s %3s\n", $5, $8, $2, $3}')
+            rev_history="(last edit: ${history})"
+        fi
+        echo "Should delete: $FILE $rev_history"
     fi
 done;
